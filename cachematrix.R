00b1a00f69a22d40962891a2f27de2f133ba6ee4
Sonny Singh <Sonny@dhcp-169-233-173-176.cruznet.ucsc.edu>
2016-03-12 16:35:27 -0800

    Save my files

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Tanpreet Gambhir
+## These functions are able to compute and cache the inverse of a matrix. The first function
+## stores the matrix and its inverse while the other checks and returns the cached inverse
+## or computes it and sets in the cache
 
-## Write a short comment describing this function
+## Function creates a 'special' matrix object which stores the set and get values of the passed matrix,
+## and its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inverse <- NULL #placeholder
+        set <- function(y) {
+                x<<-y #assign new matrix value
+                inverse<<-NULL #set to null
+        }
+        get <- function() x #return matrix
+        setinverse <-function(x) inverse <<- solve(x) #finds inverse of x
+        getinverse <-function() inverse #returns inverse of x
+        list(set=set, get=get, setinverse=setinverse, getinverse=getinverse) #special matrix object
 }
 
 
-## Write a short comment describing this function
+## This function first checks if the inverse is already stored in the cache and return it from
+## the cache if it is.
+## If not, the inverse is calculated, and sets it in the cache as well (by calling the setinverse)
+## The inverse is then returned
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inverse <- x$getinverse #cached inverse
+        if(!is.null(inverse)){ #check if cached inverse exists
+                message("Getting cached matrix inverse")
+                return(inverse) #returns cached inverse
+        }
+        data<-x$get() #passes new matrix to cache
+        inverse <- solve(data) #find matrix inverse
+        x$setinverse(inverse) #set matrix inverse to cache
+        inverse #return inverse
 }
+
